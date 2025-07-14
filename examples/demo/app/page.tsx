710de9aeea900291d4897aa70a5edab4ba7cd782
joshkrutz <46111059+joshkrutz@users.noreply.github.com>
2025-07-13 21:30:42 -0700

    Final touches for styling

--- examples/demo/app/page.tsx
+++ examples/demo/app/page.tsx
@@ -7,24 +7,39 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
+          className="dark:invert self-center"
           src="/sasquash.svg"
           alt="Next.js logo"
           width={180}
           height={180}
           priority
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Populate a .env file with your GITHUB_TOKEN, GITHUB_REPO,
-            GITHUB_OWNER
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              pnpm add @joshkrutz/sasquash
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">instructions 2...</li>
-        </ol>
+        <div>
+          <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+            <li className="mb-2 tracking-[-.01em]">
+              {'Create a '}
+              <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+                .env.local
+              </code>
+              {' file at the root of your test app'}
+            </li>
+            <li className="tracking-[-.01em]">
+              Populate
+              <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+                GITHUB_TOKEN
+              </code>
+              {', '}
+              <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+                GITHUB_REPO_NAME
+              </code>
+              {', and '}
+              <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+                GITHUB_REPO_OWNER
+              </code>
+            </li>
+            <li>Use the "Report an issue" button to test out the basic API.</li>
+          </ol>
+        </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
